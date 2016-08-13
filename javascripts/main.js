73e8b2c23efb4fb9a7f4ff332b8a48c3dac5d08e
crcons <repolhosuicida@gmail.com>
2016-08-13 00:28:05 -0300

    Added the getIngredients function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,43 @@
-console.log('This would be the main JS file.');
+function getIngredients(full_name)
+{
+  foods = ["coco", "cenoura", "repolho", "casca"];
+  good_foods = [];
+
+  for (i = 0; i < foods.length; i++)
+  {
+    food = foods[i];
+    used_letters = Array.apply(null, Array(full_name.length).map(Number.prototype.valueOf,0);
+    rotten = 0;
+    for (j = 0; j < food.length; j++)
+    {
+      letter = food[j];
+      okay = 0;
+      subname = full_name;
+      while (subname.indexOf(letter) > -1)
+      {
+        where = subname.indexOf(letter);
+        if (used_letters[where] == 1)
+        {
+          subname = subname.slice(where+1);
+        }
+        else
+        {
+          used_letters[where] = 1;
+          okay = 1;
+          break;
+        }
+      }
+      if (!okay)
+      {
+        rotten = 1;
+      }
+    }
+    if (!rotten)
+    {
+      good_foods.push(food);
+      document.getElementById("ingredients").innerHTML += food + " ";
+    }
+  }
+  
+  return good_foods;
+}
