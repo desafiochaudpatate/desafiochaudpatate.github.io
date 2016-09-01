a32064446e66ee8663cec3aa80675a4f5bafd26a
crcons <repolhosuicida@gmail.com>
2016-09-01 17:01:04 -0300

    removed unused method

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,41 +1,27 @@
-function handleQueryResponse2(response) {
+function handleQueryResponse(response)
+{
       alert('handling');
       if (response.isError()) {
         alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
         return;
       }
-    
+      
       var data = response.getDataTable();
       alert(data);
       visualization = new google.visualization.Table(document.getElementById('ingredients'));
       visualization.draw(data, null);
-  }
-  
-function onLoadCallback()
-{
-      // Define the chart to be drawn.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Element');
-      data.addColumn('number', 'Percentage');
-      data.addRows([
-        ['Nitrogen', 0.78],
-        ['Oxygen', 0.21],
-        ['Other', 0.01]
-      ]);
-
-      // Instantiate and draw the chart.
-      var chart = new google.visualization.PieChart(document.getElementById('ingredients'));
-      chart.draw(data, null);
 }
 
 function getIngredients(full_name)
 {
+  google.charts.load('current', {packages: ['corechart', 'table']});
+  
   //foods = ["coco", "cenoura", "repolho", "pão", "massa", "cereal", "gordura", "vegano", "vegana", "vegetariano", "vegetariana", "pancs", "rosa", "mandioca", "carne", "tâmara", "doce", "marisco", "tomate", "damasco", "nectarina", "erva-doce", "codorna", "doce", "vaca", "mate", "anis", "amora", "menta", "osso", "risoto", "ova", "ovo", "torta", "caviar", "morno", "cookie", "cheesecake", "tacos", "amido", "endro", "mostarda", "cítrico", "mandarina", "assado", "carneiro", "cordeiro", "ovelha", "pizza", "acre", "nata", "amêndoa", "aveia", "coca", "coca-cola", "ostra", "ricota", "vietnã", "vieira", "camarão", "corada", "corado", "iogurte", "síria", "canadá", "mel", "melado", "melada", "coentro", "arder", "pimenta", "chá", "especiaria", "índia", "cone", "purê", "maçã", "carolina", "profiterole", "negro", "preto", "sal", "cru", "aro", "coral", "licor", "pinot", "suco", "pasta", "copa", "pato", "prato", "sopa", "pé", "óleo", "lata", "canola", "louro", "praia", "cortar", "canela", "pote", "rolo", "cuia", "risole", "pera", "picles", "aipo", "panceta", "jabuticaba", "panc", "polenta", "naan", "calor", "seco", "porco", "torrar", "porto", "sagu", "roma", "romã", "rosca", "amendoim", "manjericão", "pesto", "cru", "isca", "nigéria", "rã", "guaco", "arruda", "grécia", "rússia", "sugar", "siri", "caranguejo", "pimenta", "orégano", "suécia", "regar", "cupim", "aipim", "mali", "estados unidos", "panela", "faca", "frio", "fogo", "madalena", "mandolim", "cifão", "anel", "liso", "lisa", "azul", "anil"];
   alert('before');
   var url='https://docs.google.com/spreadsheets/d/1rgFHL4Mxkio-l5mcI8kDtn8k5QzmOs2MG-0g58N3j5I/gviz/tq?tq=select+A'
   alert(url);
   var query = new google.visualization.Query(url);
-  query.send(handleQueryResponse2);
+  query.send(handleQueryResponse);
   //TODO: add following word groups to the list: fruits and vegetables, plates, edible animals, countries, colors, edible fungi,
   //spices, diet types, textures, shapes, kitchen tools
   //TODO: disregard graphic accentuation (including the tilde and cedilla)
