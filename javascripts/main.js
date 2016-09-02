bee56d8dac5b6e3a101f0ffc94ef40c8bf64f0ee
crcons <repolhosuicida@gmail.com>
2016-09-02 13:58:43 -0300

    separating methods and adding a lot of todos

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,10 @@
 foods = [];
-//TODO: getIngredients should only call the db; handleQueryResponse will call a third method to go through the arrays and return the good_foods to the page
+fullName = "";
 
 function handleQueryResponse(response)
 {
+  //TODO: lose the first result of each column (title)
+  //TODO: disregard "null"
   if (response.isError()) {
     alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
     return;
@@ -10,20 +12,27 @@ function handleQueryResponse(response)
   
   var data = response.getDataTable();
   foods = data.getDistinctValues(0);
-  //visualization = new google.visualization.Table(document.getElementById('ingredients'));
-  //visualization.draw(data, null);
+  getGoodFoods();
 }
 
 function getIngredients(full_name)
 {
+  //TODO: add a "select language" field
+  //TODO: add a field to specify quantity of results to show on screen
+  //TODO: get specific sheet on document, according to selected language
+  //TODO: get ingredients from different columns
+  fullName = full_name;
   //foods = ["coco", "cenoura", "repolho", "pão", "massa", "cereal", "gordura", "vegano", "vegana", "vegetariano", "vegetariana", "pancs", "rosa", "mandioca", "carne", "tâmara", "doce", "marisco", "tomate", "damasco", "nectarina", "erva-doce", "codorna", "doce", "vaca", "mate", "anis", "amora", "menta", "osso", "risoto", "ova", "ovo", "torta", "caviar", "morno", "cookie", "cheesecake", "tacos", "amido", "endro", "mostarda", "cítrico", "mandarina", "assado", "carneiro", "cordeiro", "ovelha", "pizza", "acre", "nata", "amêndoa", "aveia", "coca", "coca-cola", "ostra", "ricota", "vietnã", "vieira", "camarão", "corada", "corado", "iogurte", "síria", "canadá", "mel", "melado", "melada", "coentro", "arder", "pimenta", "chá", "especiaria", "índia", "cone", "purê", "maçã", "carolina", "profiterole", "negro", "preto", "sal", "cru", "aro", "coral", "licor", "pinot", "suco", "pasta", "copa", "pato", "prato", "sopa", "pé", "óleo", "lata", "canola", "louro", "praia", "cortar", "canela", "pote", "rolo", "cuia", "risole", "pera", "picles", "aipo", "panceta", "jabuticaba", "panc", "polenta", "naan", "calor", "seco", "porco", "torrar", "porto", "sagu", "roma", "romã", "rosca", "amendoim", "manjericão", "pesto", "cru", "isca", "nigéria", "rã", "guaco", "arruda", "grécia", "rússia", "sugar", "siri", "caranguejo", "pimenta", "orégano", "suécia", "regar", "cupim", "aipim", "mali", "estados unidos", "panela", "faca", "frio", "fogo", "madalena", "mandolim", "cifão", "anel", "liso", "lisa", "azul", "anil"];
-  foods = [];
   var url='https://docs.google.com/spreadsheets/d/1rgFHL4Mxkio-l5mcI8kDtn8k5QzmOs2MG-0g58N3j5I/gviz/tq?tq=select+A'
   var query = new google.visualization.Query(url);
   query.send(handleQueryResponse);
+}
+
+function getGoodFoods()
+{
   //TODO: add following word groups to the list: fruits and vegetables, plates, edible animals, countries, colors, edible fungi,
   //spices, diet types, textures, shapes, kitchen tools
-  //TODO: disregard graphic accentuation (including the tilde and cedilla)
+  //TODO: disregard graphic accentuation (including the tilde and cedilla and ')
   //TODO: crawl the web (maybe wikipedia is enough?) getting food descriptions and model the documents to get only food-related words
   document.getElementById("ingredients").innerHTML = "";
   good_foods = [];
@@ -37,7 +46,7 @@ function getIngredients(full_name)
     {
       letter = food[j];
       okay = 0;
-      subname = full_name;
+      subname = fullName;
       while (subname.indexOf(letter) > -1)
       {
         where = subname.indexOf(letter);
